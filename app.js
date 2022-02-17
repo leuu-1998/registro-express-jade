7711a19a50f733874f027656c1ea02b58bd4e017
leuu-1998 <lmoinah@uni.pe>
2022-02-17 14:52:16 -0500

    last commit 3

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var registerRouter = require("./routes/register");
 
 var app = express();
@@ -34,7 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/register", registerRouter);
 
 // catch 404 and forward to error handler
